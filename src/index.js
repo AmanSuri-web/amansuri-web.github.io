d73f35e3afdabcc863daa860d688a213b29b6fe5
aman suri <amansuri@amans-MacBook-Pro.local>
2022-06-13 23:20:01 +0530

    browserRouter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
 import App from './App.js';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 
 ReactDOM.render(
 				<>
-				<HashRouter>
+				<BrowserRouter>
 				<App/>
-				</HashRouter>
+				</BrowserRouter>
 				</>, 
 	document.getElementById("root") ); // this is always fixed and it points to the index.html file
 
