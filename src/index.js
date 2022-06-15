629fe827c515a486b04617b1312dd7296807ab08
aman suri <amansuri@amans-MacBook-Pro.local>
2022-06-15 13:34:28 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import 'core-js/actual';
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
 import App from './App.js';
 import {HashRouter} from "react-router-dom";
 
-const Artists = lazy(() => import("./App.js"))
+
 ReactDOM.render(
 				<>
 				<HashRouter>
-					<Suspense fallback={<h1>Still Loading…</h1>}>
-						<Artists/>
-					</Suspense>
+					
+						<App/>
+					
 				</HashRouter>
 				</>, 
 	document.getElementById("root") ); // this is always fixed and it points to the index.html file
